de5aaf403de459f4551a73e12a5196b5c6989198
chenhuage <89718325+chenhuage@users.noreply.github.com>
2025-08-07 15:28:03 +0500

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,12 +5,13 @@ import os
 
 # 目标URL列表
 urls = [
-    'https://ip.164746.xyz', 
-    'https://cf.090227.xyz', 
-
+    
+    'https://www.wetest.vip/page/cloudflare/address_v4.html'
 ]
  #   'https://stock.hostmonit.com/CloudFlareYes',
- #   'https://www.wetest.vip/page/cloudflare/address_v4.html'
+ # 'https://ip.164746.xyz', 
+ #   'https://cf.090227.xyz', 
+
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
 
